bded1c596ad83c5fa02bbd558835ade76526ad57
Siddhesh M <SBMPC.Student@SVKMGRP.COM>
2026-01-13 13:42:51 +0530

    odd even commit

--- week_1/demo.py
+++ week_1/demo.py
@@ -1,8 +1,33 @@
 def greet():
     print("Hi!!! Hello World!!!")
 
+def getNumber():
+    number = int(input("Enetr a number: "))
+    return number
+
+def checkOddEven(num):
+    return bool(num % 2)
+
+def getFactorial(num):
+    res = 1
+    for num in range(1, num+1):
+        res = res * num
+    return res
+
+def countNumberOfDigits(num):
+    pass
+
 def main():
     greet()
+    print("Enter a number for factorial.")
+    number = getNumber()
+    result = getFactorial(number)
+    print(f"factorial of {number} is {result}")
+    # for num in range(1,upper_limit+1):
+    #     if checkOddEven(num) % 2 :
+    #         print("{} - odd.".format(num))
+    #     else:
+    #         print(f"{num} - even.")
 
 if __name__ == "__main__":
     main()
